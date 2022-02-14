8e7c44559803f6eaf1a29b4e6a974c96298bc29a
ddddddeon <opfunk@gmail.com>
2022-02-13 22:03:11 -0500

    readability tweaks

--- src/dcrypt.c
+++ src/dcrypt.c
@@ -6,13 +6,12 @@
 #include <openssl/rsa.h>
 #include <string.h>
 
-#define CHECK_EQUALS(val, func, handle)                                  \
-  do {                                                                   \
-    int ret = (func);                                                    \
-    if (ret != val) {                                                    \
-      printf("%s (%s:%d: %s)\n", "Error at", __FILE__, __LINE__, #func); \
-      handle;                                                            \
-    }                                                                    \
+#define CHECK_EQUALS(val, ret, handle)                        \
+  do {                                                        \
+    if (ret != val) {                                         \
+      printf("%s (%s:%d)\n", "Error at", __FILE__, __LINE__); \
+      handle;                                                 \
+    }                                                         \
   } while (0)
 
 #define CHECK_NULL(func, message, retval)                             \
@@ -34,7 +33,10 @@ EVP_PKEY *GenerateKey() {
   EVP_PKEY_CTX *ctx = NULL;
   ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, NULL);
   CHECK_NULL(ctx, "Could not create EVP_PKEY context", NULL);
-  CHECK_EQUALS(1, EVP_PKEY_keygen_init(ctx), return NULL);
+
+  int ret = EVP_PKEY_keygen_init(ctx);
+  CHECK_EQUALS(1, ret, return NULL);
+
   EVP_PKEY_CTX_set_rsa_keygen_bits(ctx, 2048);
   EVP_PKEY *key = NULL;
   EVP_PKEY_keygen(ctx, &key);
@@ -43,29 +45,29 @@ EVP_PKEY *GenerateKey() {
 }
 
 bool SetKey(BIO *bio, EVP_PKEY *key, bool is_private) {
+  int ret = 0;
   if (is_private) {
-    CHECK_EQUALS(1, PEM_write_bio_PrivateKey(bio, key, NULL, NULL, 0, 0, NULL),
-                 return false);
+    ret = PEM_write_bio_PrivateKey(bio, key, NULL, NULL, 0, 0, NULL);
   } else {
-    CHECK_EQUALS(1, PEM_write_bio_PUBKEY(bio, key), return false);
+    ret = PEM_write_bio_PUBKEY(bio, key);
   }
-
+  CHECK_EQUALS(1, ret, return false);
   return true;
 }
 
 EVP_PKEY *GetKey(BIO *bio, bool is_private) {
   RSA *rsa = NULL;
   if (is_private) {
-    CHECK_NULL((PEM_read_bio_RSAPrivateKey(bio, &rsa, NULL, NULL)),
-               "Could not read private key from BIO", NULL);
+    PEM_read_bio_RSAPrivateKey(bio, &rsa, NULL, NULL);
   } else {
-    CHECK_NULL((PEM_read_bio_RSA_PUBKEY(bio, &rsa, NULL, NULL)),
-               "Could not read public key from BIO", NULL);
+    PEM_read_bio_RSA_PUBKEY(bio, &rsa, NULL, NULL);
   }
+  CHECK_NULL(rsa, "Could not read key from BIO", NULL);
 
   EVP_PKEY *key = NULL;
   key = EVP_PKEY_new();
   CHECK_NULL(key, "Could not allocat EVP_PKEY", NULL);
+
   EVP_PKEY_assign_RSA(key, rsa);
   return key;
 }
@@ -74,18 +76,23 @@ bool KeyToFile(EVP_PKEY *key, char *out_file, bool is_private) {
   BIO *file_BIO = NULL;
   file_BIO = BIO_new_file(out_file, "w");
   CHECK_NULL(file_BIO, "Could not load file for writing", false);
-  CHECK_EQUALS(true, SetKey(file_BIO, key, is_private), BIO_free(file_BIO);
-               return false);
+
+  int ret = SetKey(file_BIO, key, is_private);
   BIO_free(file_BIO);
+  CHECK_EQUALS(true, ret, return false);
+
   return true;
 }
 
 unsigned char *KeyToString(EVP_PKEY *key, bool is_private) {
+  int ret = 0;
   BIO *key_BIO = NULL;
   key_BIO = BIO_new(BIO_s_mem());
   CHECK_NULL(key_BIO, "Could not allocate memory for writing", NULL);
-  CHECK_EQUALS(1, SetKey(key_BIO, key, is_private), BIO_free(key_BIO);
-               return NULL);
+
+  ret = SetKey(key_BIO, key, is_private);
+  CHECK_EQUALS(1, ret, BIO_free(key_BIO); return NULL);
+
   int key_length = BIO_pending(key_BIO);
   unsigned char *key_string = (unsigned char *)malloc(key_length + 1);
   CHECK_NULL(key_string, "Could not allocate memory for key string", NULL);
@@ -104,10 +111,12 @@ EVP_PKEY *FileToKey(char *in_file, bool is_private) {
   BIO *file_BIO = NULL;
   file_BIO = BIO_new_file(in_file, "r");
   CHECK_NULL(file_BIO, "Could not open file for reading", NULL);
+
   EVP_PKEY *key = NULL;
   key = GetKey(file_BIO, is_private);
   BIO_free(file_BIO);
   CHECK_NULL(key, "Could not get key from file", NULL);
+
   return key;
 }
 
@@ -115,10 +124,12 @@ EVP_PKEY *StringToKey(unsigned char *key_string, bool is_private) {
   BIO *key_BIO = NULL;
   key_BIO = BIO_new_mem_buf(key_string, -1);
   CHECK_NULL(key_BIO, "Could not allocate memory buffer for string", NULL);
+
   EVP_PKEY *key = NULL;
   key = GetKey(key_BIO, is_private);
   BIO_free(key_BIO);
   CHECK_NULL(key, "Could not get key from string", NULL);
+
   return key;
 }
 
@@ -166,8 +177,10 @@ bool Verify(char *message, unsigned char *signature, EVP_PKEY *pubkey) {
 unsigned char *GenerateRandomBytes(int size) {
   unsigned char *bytes = (unsigned char *)malloc(size);
   CHECK_NULL(bytes, "Could not allocate memory for random bytes", NULL);
+
   int written = RAND_bytes(bytes, size);
   CHECK_EQUALS(1, written, return NULL);
+
   bytes[size - 1] = '\0';
   return bytes;
 }
